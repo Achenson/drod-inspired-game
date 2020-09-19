38faadd8a9d6f6589420478413d6515b5806b5b7
Achenson <ach.01.mail@gmail.com>
2020-09-19 17:05:02 +0200

    newGame btn proto

--- src/components/NewGameBtn.tsx
+++ src/components/NewGameBtn.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 function NewGameBtn({}: Props): JSX.Element{
     return (
-        <div className="text-center my-2">
-           <button>
-               New game
+        <div className="text-center my-4" style={{fontSize: "0.7rem" }} >
+           <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-sm">
+               NEW GAME
            </button>
         </div>
     )
