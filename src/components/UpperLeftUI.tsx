2c828ab92a9f71bdca24bb31e00af71acdfdab4a
achenson <45999022+Achenson@users.noreply.github.com>
2020-09-22 12:40:18 +0200

    medals being awarded

--- src/components/UpperLeftUI.tsx
+++ src/components/UpperLeftUI.tsx
@@ -9,12 +9,39 @@ interface Props {
 }
 
 function UpperLeftUI({ enemiesKilled, topScore, enemies }: Props): JSX.Element {
+
+
+  let medalColor;
+
+  let topScoreInt = parseInt(topScore as string)
+
+  if (topScoreInt < 100) {
+    medalColor="black"
+  }
+
+  if (topScoreInt >= 100 && topScoreInt < 175) {
+    medalColor="#cd7f32"
+  }
+
+  if (topScoreInt >= 175 && topScoreInt < 250) {
+    medalColor=" #c0c0c0"
+  }
+
+  if (topScoreInt >= 250) {
+    medalColor="gold"
+  }
+
+
+
+
+
+
   return (
     <div>
       <p>Top score</p>
       <div className="flex justify-around mb-1">
-        <p className="text-center font-droid-serif text-lg">{topScore}23</p>
-        <Medal className="h-6 fill-current text-gray-900" />
+        <p className="text-center font-droid-serif text-lg">{topScore}</p>
+        <Medal className="h-6 fill-current" style={{color: `${medalColor}`}} />
       </div>
 
       {/* <p>kills: {enemiesKilled}</p> */}
