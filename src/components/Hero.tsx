0f28c4e1fd3a2de0d09ca706c402feafb1fc0118
achenson <45999022+Achenson@users.noreply.github.com>
2020-10-14 16:38:37 +0200

    Hero new look2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ function Hero({ heroDirection, triangleMargins }: Props): JSX.Element {
     width: "30px",
     // more than 40% - hero is not rendered properly on chrome
     // borderRadius: "40%",
-    borderRadius: "40%"
+    borderRadius: "40%",
     // marginTop: "auto"
   };
 
@@ -60,17 +60,21 @@ function Hero({ heroDirection, triangleMargins }: Props): JSX.Element {
         </div>
         <div
           className={`w-3 h-3 bg-yellow-500 rounded-full z-40`}
-          style={{ position: "absolute", top: "11px", left: "-3px" }}
+          style={{ position: "absolute", top: "12px", left: "-3px" }}
         >
           <div style={halfACircle}></div>
         </div>
         <div
-        className="w-6 h-4 absolute rounded-lg z-30"
-        style={{ backgroundColor: "green", top: "9px", left: "-10px", width: "26px", height: "18px"  }}
-        >
-
-        </div>
-
+          className="absolute z-30"
+          style={{
+            backgroundColor: "green",
+            top: "8px",
+            left: "-10px",
+            width: "26px",
+            height: "20px",
+            borderRadius: "9px"
+          }}
+        ></div>
       </div>
     </div>
   );
