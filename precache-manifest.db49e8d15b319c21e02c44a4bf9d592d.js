390809088b36ad4368d3f085eede50de51f74d6c
Andrzejek <ach.01.mail@gmail.com>
2020-11-01 21:56:10 +0100

    Updates

---
+++ precache-manifest.db49e8d15b319c21e02c44a4bf9d592d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd9aee84f14014bbc6cae19934cc950",
+    "url": "/drod-inspired-game/index.html"
+  },
+  {
+    "revision": "dba35ee000bc6457b88c",
+    "url": "/drod-inspired-game/static/css/main.07e2b3a7.chunk.css"
+  },
+  {
+    "revision": "3838d35ffa2c2ed6a662",
+    "url": "/drod-inspired-game/static/js/2.51685509.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drod-inspired-game/static/js/2.51685509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba35ee000bc6457b88c",
+    "url": "/drod-inspired-game/static/js/main.a7c9ead1.chunk.js"
+  },
+  {
+    "revision": "f703590f6d640b373e1c",
+    "url": "/drod-inspired-game/static/js/runtime-main.3b6bbdde.js"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/drod-inspired-game/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "5f56fcc5617a94054ac625b8889e346c",
+    "url": "/drod-inspired-game/static/media/arrowUp.5f56fcc5.svg"
+  },
+  {
+    "revision": "2eba3f3efb2f460cc47b6c83c33b623d",
+    "url": "/drod-inspired-game/static/media/award_nylon_plucked.2eba3f3e.mp3"
+  },
+  {
+    "revision": "f6a29680632ca98f23215f27e0e19f7c",
+    "url": "/drod-inspired-game/static/media/backward.f6a29680.svg"
+  },
+  {
+    "revision": "9aca549f95e0b6b2e9d47ce6fb3f51f9",
+    "url": "/drod-inspired-game/static/media/bite_munch.9aca549f.mp3"
+  },
+  {
+    "revision": "6da81e592f017ea5c2bcf84aab1e5971",
+    "url": "/drod-inspired-game/static/media/cancel.6da81e59.svg"
+  },
+  {
+    "revision": "4d23c9ca47530b975c09d30b891b6ae7",
+    "url": "/drod-inspired-game/static/media/cog-small.4d23c9ca.svg"
+  },
+  {
+    "revision": "85ca9f1269d2e8c076c9084b33f2c9f5",
+    "url": "/drod-inspired-game/static/media/confirm.85ca9f12.svg"
+  },
+  {
+    "revision": "2c3d21b7783c004ae9d9532b57f27510",
+    "url": "/drod-inspired-game/static/media/curved-arrow-2261.2c3d21b7.svg"
+  },
+  {
+    "revision": "d7f65f97a7d265ef423bc5723279f538",
+    "url": "/drod-inspired-game/static/media/deleteDanger.d7f65f97.svg"
+  },
+  {
+    "revision": "5a836b8a3b9502fe97be0e4f915032bd",
+    "url": "/drod-inspired-game/static/media/game_error.5a836b8a.mp3"
+  },
+  {
+    "revision": "968e5199591a0724824a6abcc68feb18",
+    "url": "/drod-inspired-game/static/media/impact_sword_hit.968e5199.mp3"
+  },
+  {
+    "revision": "60635ad7121782a0f3b222af5b0ef853",
+    "url": "/drod-inspired-game/static/media/keyboard.60635ad7.svg"
+  },
+  {
+    "revision": "0c6ada01f587f72c37e822b92b2f35e3",
+    "url": "/drod-inspired-game/static/media/malware-virus.0c6ada01.svg"
+  },
+  {
+    "revision": "4b531a8297b409e144937c1aa8b63b4f",
+    "url": "/drod-inspired-game/static/media/medal.4b531a82.svg"
+  },
+  {
+    "revision": "15a901e5d7e1b889e24b3dd5b1ab792b",
+    "url": "/drod-inspired-game/static/media/question-mark-round.15a901e5.svg"
+  },
+  {
+    "revision": "dd991026635c89710c7fd7bb368e9a2a",
+    "url": "/drod-inspired-game/static/media/retro_fanfare.dd991026.mp3"
+  },
+  {
+    "revision": "4856732fdffb087b1bb8e52140ac1c06",
+    "url": "/drod-inspired-game/static/media/sabre_swing.4856732f.mp3"
+  },
+  {
+    "revision": "01f68f8832af4a17c404227356e64965",
+    "url": "/drod-inspired-game/static/media/sand-clock-half.01f68f88.svg"
+  },
+  {
+    "revision": "7445b9c44638b7c95f7c9f5b502c098b",
+    "url": "/drod-inspired-game/static/media/single_step_heavy.7445b9c4.mp3"
+  },
+  {
+    "revision": "5508cd6164b292d9e5cce977b0628135",
+    "url": "/drod-inspired-game/static/media/skull.5508cd61.svg"
+  },
+  {
+    "revision": "cd0e8aeb8e28c8dc94692f3a021d970e",
+    "url": "/drod-inspired-game/static/media/snore_single.cd0e8aeb.mp3"
+  },
+  {
+    "revision": "e75833199bda2440ad671cec0b732d93",
+    "url": "/drod-inspired-game/static/media/sword.e7583319.svg"
+  },
+  {
+    "revision": "cca0e152b3e730f89a5316b8d5cd6e26",
+    "url": "/drod-inspired-game/static/media/volumeOff.cca0e152.svg"
+  }
+]);
